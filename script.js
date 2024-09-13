3e88d293b109ebec9e0e18e6cc68a6ea7c87467d
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-13 23:01:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ let ln = document.getElementById("lname");
 let pn = document.getElementById("pnum");
 let em = document.getElementById("email");
 let btn = document.getElementById("button");
-btn.addEventListener("click", function() {
-	alert("First Name: " + fn + 
-          "\nLast Name: " + ln + 
-          "\nPhone Number: " + pn + 
-          "\nEmail ID: " + em);
+btn.addEventListener("click", function(event) {
+	event.preventDefault();
+	alert("First Name: " + fn.value + 
+          "\nLast Name: " + ln.value + 
+          "\nPhone Number: " + pn.value + 
+          "\nEmail ID: " + em.value);
 )
 });
