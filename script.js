a09231c9db7a6720763a9c7e469b4c97ff6e927f
Meghna Singh <138083187+MeghnaSingh15@users.noreply.github.com>
2024-09-13 22:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let fn = document.getElementById("fname");
+let ln = document.getElementById("lname");
+let pn = document.getElementById("pnum");
+let em = document.getElementById("email");
+let btn = document.getElementById("button");
+btn.addEventListener("click", function() {
+	alert(First Name: fn.value
+		Last Name: ln.value
+			Phone Number: pn.value
+				Email ID: em.value)
+});
